a7f4316c8c3e6d5fc5da574b0f967ac98bb2590b
Juan Sanchez <jsanchez@symphonysensa.com>
2024-03-27 22:55:25 -0400

    fix

--- install.sh
+++ install.sh
@@ -14,18 +14,30 @@ sudo apt update
 sudo apt install zsh-syntax-highlighting
 echo "zsh-syntax-highlighting installation complete!"
 
-# Install Starship
-echo "Installing Starship..."
-curl -fsSL https://starship.rs/install.sh | bash
-echo "Starship installation complete!"
-
-# Install Fira Code font
-echo "Installing Fira Code font..."
-wget -O firacode.zip https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip
-unzip -o firacode.zip -d firacode
+# Install FiraCode Nerd Font
+echo "Installing FiraCode Nerd Font..."
+# Download and extract the font archive
 mkdir -p ~/.local/share/fonts
-cp firacode/*.ttf ~/.local/share/fonts
+wget -O /tmp/firacode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/0xProto.zip
+unzip -o /tmp/firacode.zip -d ~/.local/share/fonts/
+rm -f /tmp/firacode.zip
+# Update font cache
 fc-cache -f -v
-echo "Fira Code font installation complete!"
+echo "FiraCode Nerd Font installation complete!"
+
+# Install Oh My Zsh
+echo "Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Oh My Zsh installation complete!"
+
+# Install auto-suggestions plugin
+echo "Installing auto-suggestions plugin for Oh My Zsh..."
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# Clone zsh-syntax-highlighting plugin
+echo "Cloning zsh-syntax-highlighting plugin..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+source "$HOME/.zshrc"
 
 echo "Dotfiles installation complete!"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,13 +1,5 @@
-# Custom aliases
-alias ll='ls -alF'
-alias gs='git status'
-alias gc='git commit'
-
 # Set PATH
 export PATH="$HOME/bin:$PATH"
 
-# Enable syntax highlighting
-source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# Load Starship prompt
-eval "$(starship init zsh)"
+# Enable Oh My Zsh plugins
+plugins=(zsh-syntax-highlighting zsh-autosuggestions)
