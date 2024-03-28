3a0b1f580085a0377f324c6f7c293ee9d5379144
Juan Sanchez <14128083+dexter0323@users.noreply.github.com>
2024-03-27 21:59:53 -0400

    fix: setup 2

--- install.sh
+++ install.sh
@@ -8,6 +8,17 @@ ln -sf "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
 ln -sf "$(pwd)/git/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$(pwd)/starship/starship.toml" "$HOME/.config/starship.toml"
 
+# Install zsh-syntax-highlighting
+echo "Installing zsh-syntax-highlighting..."
+sudo apt update
+sudo apt install zsh-syntax-highlighting
+echo "zsh-syntax-highlighting installation complete!"
+
+# Install Starship
+echo "Installing Starship..."
+curl -fsSL https://starship.rs/install.sh | bash
+echo "Starship installation complete!"
+
 # Install Fira Code font
 echo "Installing Fira Code font..."
 wget -O firacode.zip https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip
